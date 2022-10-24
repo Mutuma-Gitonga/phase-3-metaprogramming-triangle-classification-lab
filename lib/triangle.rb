3f311058db10c7f5b2b99f00c97ec4137157ba23
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-24 16:33:18 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind 
+    if((@side_a > 0 && @side_b > 0 && side_c > 0) && (@side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a))
+
+      unique_triangle_sides = [@side_a, @side_b, @side_c].uniq
+      
+      case unique_triangle_sides.length
+              when 1 then :equilateral
+              when 2 then :isosceles
+              when 3 then :scalene
+              end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
